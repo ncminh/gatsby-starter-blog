f8ad1919e34e49fefb2c78196ac61b10ed599125
MINH <minh@MINHs-MacBook-Pro.local>
2022-02-21 23:29:49 +0700

    Updated MINHNC

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `MINH NC Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `MINH NC`,
+      summary: `Simple man...`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `tedncm`,
     },
   },
   plugins: [
